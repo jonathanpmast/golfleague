81886936a5bd7aeec9dbd8ff4e8a467ca7ef9c71
jonathanpmast <jonathanpmast@hotmail.com>
2021-04-15 22:25:04 -0400

    updated loader to parse xlsx and ingest golfer scores

--- golf-league-data-loader/index.mjs
+++ golf-league-data-loader/index.mjs
@@ -4,28 +4,71 @@ import dotenv from "dotenv";
 import XLSX from "xlsx";
 
 async function main() {
+    console.log("starting...");
     dotenv.config();
-    //updateConfigData();
-    readSkinsData();
+    updateConfigData();
+    let skinsData = readSkinsData();
 }
 
-async function parseParticipation(participationData) {
-
+async function parseParticipation(sourceParticipationData) {
+    let participationData = [];
+    let startParsingAt = 2;
+    let rowNum= startParsingAt;
+    for(rowNum;rowNum<sourceParticipationData.length;rowNum++) {
+        let row = sourceParticipationData[rowNum];
+        
+        participationData[rowNum-startParsingAt] = {
+            "GolferName": row["__EMPTY"],
+            "ParticipationByRound": new Array(18)
+        };
+        for(let roundNum=0;roundNum < 18; roundNum++)
+        {
+            participationData[rowNum-startParsingAt].ParticipationByRound[roundNum] = row[roundNum+1] != null ? true : false;
+        }
+    }
+    return participationData;
 }
 
-async function parseScoreData(scoreDataSheetExport) {
-
+async function parseScoreData(sourceScoreData, roundNum) {
+    let scoreColumnOffset = 3
+    let scoreData = {
+        "RoundNumber": roundNum,
+        "GolferScores":[]
+    }
+    let golferCount=0;
+    for(let i=0;i<sourceScoreData.length;i++) {
+        let row = sourceScoreData[i];
+        if(row[1] !== null && row[1] === "X")
+        {
+            scoreData.GolferScores[golferCount] = {
+                "GolferName" : row[0],
+                "Handicap": row[2],
+                "Scores" :[]
+            };
+            for(let holeNum = 0; holeNum < 18; holeNum++) 
+            {
+                scoreData.GolferScores[golferCount].Scores[holeNum] = row[holeNum+scoreColumnOffset] ?? 0;
+            }
+            golferCount += 1;
+        }
+        //if(row[0] !== null || row[0] !== "Golfer" || row[0] !== "Net")
+    }
+    return scoreData;
 }
+
 async function readSkinsData() {
     const workbookpath = process.env.SKINS_WORKBOOK;
     var workbook = XLSX.readFile(workbookpath);
-    let participationData = XLSX.utils.sheet_to_json(workbook.Sheets["Skins By Week"]);
-    let scoreData = new Array();
+    let skinsData = {};    
+    skinsData["ParticipationData"] = await parseParticipation(XLSX.utils.sheet_to_json(workbook.Sheets["Skins By Week"]));
+    skinsData.RoundData=[];
     for(var i=1; i<19; i++)
     {
-        scoreData.push(XLSX.utils.sheet_to_json(workbook.Sheets["Skins "+i]));
+        let scores = await parseScoreData(XLSX.utils.sheet_to_json(workbook.Sheets["Skins "+i],{header:1}),i);
+        
+        skinsData.RoundData.push(scores);
     }
-    
+    return skinsData;
 }
 
 async function updateConfigData() {
@@ -36,6 +79,10 @@ async function updateConfigData() {
     const response = await axios.post(localConfigUrl,json);
 
     console.log(response.statusText);
+}
+
+async function updateSkinsData(skinsData) {
+    
 }
 
 async function loadConfigData() {
