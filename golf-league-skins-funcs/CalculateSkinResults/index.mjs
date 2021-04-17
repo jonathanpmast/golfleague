ec281ee08ca4ca548b1947640466d5e7a9372255
jonathanpmast <jonathanpmast@hotmail.com>
2021-04-17 16:15:16 -0400

    added comments and some additional metadata for skinResults

--- golf-league-skins-funcs/CalculateSkinResults/index.mjs
+++ golf-league-skins-funcs/CalculateSkinResults/index.mjs
@@ -1,41 +1,47 @@
+// assumes it's a 9 hole round and takes the skinResults data set, 
+// looping through each golfer/hole and setting flags if a golfer canceled or won a skin
 function calculateSkins(skinResults) {
     for(let i = 0; i < 9; i++) {
         var minScore = getMinScoreForHole(skinResults, i);
         if(minScore.golfersWithScore.length > 1)
         {
             for(let j = 0; j<minScore.golfersWithScore.length; j++){
-                skinResults[minScore.golfersWithScore[j]][i].cancelSkin=true;
+                skinResults.results[minScore.golfersWithScore[j]][i].cancelSkin=true;
             }
         }
         else{
-            skinResults[minScore.golfersWithScore[0]][i].isSkin = true;
+            skinResults.results[minScore.golfersWithScore[0]][i].isSkin = true;
         }
     }
 
     return skinResults;
 }
 
+// finds the minimum score for a hole and records which golfers had the score
 function getMinScoreForHole(skinResults, holeIndex) {
     let output = {
         minScore: 11,
         golfersWithScore: []
     };
-    for(let i in skinResults) {
-        if(i !== "id" && i !== "roundYear") { 
-            const score = skinResults[i][holeIndex].net;
-            if(score === output.minScore)
-                output.golfersWithScore.push(i);
-            if(score < output.minScore) {
-                output.golfersWithScore.length=0;
-                output.golfersWithScore.push(i);
-                output.minScore = score;
-            }
+    for(let i in skinResults.results) {
+        
+        const score = skinResults.results [i][holeIndex].net;
+        if(score === output.minScore)
+            output.golfersWithScore.push(i);
+        if(score < output.minScore) {
+            output.golfersWithScore.length=0;
+            output.golfersWithScore.push(i);
+            output.minScore = score;
         }
-    }
+        
+    } 
     return output;
 }
 
-function getHoleResults(golferScore, scores, strokeIndexes) {
+// pulls data out of the vanilla score results and adds them to a data object
+// that supports additional metadata for calculating, recording, and displaying
+// skin results
+function getHoleResults(golferScore, scores, strokeIndexes, startHole) {
     let holeResults = [];
     for(let scoreIdx = 0; scoreIdx< scores.length; scoreIdx++)
     {
@@ -44,12 +50,13 @@ function getHoleResults(golferScore, scores, strokeIndexes) {
             net: strokeIndexes[scoreIdx] <= golferScore.handicap ? scores[scoreIdx] - 1 : scores[scoreIdx],
             isSkin: false,
             cancelSkin: false,
-            holeNumber: scoreIdx+1
+            holeNumber: scoreIdx+startHole
         };
     }
     return holeResults;
 }
-
+// fills up and returns an array with each stroke index 
+// for the holes being played in the round
 function getStrokeIndexes(holes,scoreData) {
     let strokeIndexes = [];
     for(let j = 0; j < 9; j++) {
@@ -58,6 +65,10 @@ function getStrokeIndexes(holes,scoreData) {
     return strokeIndexes;
 }
 
+// receives vanilla score data for a set of golfers for a given round in a random golf league in the 'documents' parameter
+// the data is a json document which, in th is case, is sourced out of an Azure Cosmos DB, additionally receives a 
+// configuration document in the "golfLeagueConfig" parameter that defines things like the stroke index for a
+// given hole
 export default async function (context, documents, golfLeagueConfig) {
     let skinResults = [];
     if (!!documents && documents.length > 0) {
@@ -69,12 +80,13 @@ export default async function (context, documents, golfLeagueConfig) {
             skinResults[i] = {};  
             skinResults[i].id = scoreData.id;
             skinResults[i].roundYear = scoreData.roundYear;
-            // { gross: int, net: int, isSkin:bool, cancelSkin:bool }
+            skinResults[i].startHole = scoreData.startHole;
+            skinResults[i].results={};
             for(let k = 0; k < golferScores.length; k++) {
                 const golferScore = golferScores[k];
                 const scores = golferScore.scores;
                 
-                skinResults[i][golferScore.golferName] = getHoleResults(golferScore,scores,strokeIndexes);   
+                skinResults[i].results[golferScore.golferName] = getHoleResults(golferScore,scores,strokeIndexes,scoreData.startHole);   
             }        
             skinResults[i] = calculateSkins(skinResults[i]);
         }
