427846da35fa429a9e491d754228d199af004c8a
Jonathan Mast <jonathanpmast@hotmail.com>
2021-06-09 13:43:43 -0400

    formattaing updates for skins view

--- golfleague-ui/src/components/SkinResult.vue
+++ golfleague-ui/src/components/SkinResult.vue
@@ -1,68 +1,70 @@
 <template>
-  <table
-    v-if="skinResultData"
-    class="table-auto border-collapse text-sm"
-  >
-    <thead>
-      <tr class="border-b">
-        <th />
-        <th class="text-center px-1">
-          H
-        </th>
-        <th
-          v-for="n in 9"
-          :key="n"
-          class="text-center px-1"
-        >
-          {{ skinResultData.startHole + n - 1 }}
-        </th>
-        <th class="text-center px-1">
-          T
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <template
-        v-for="(result,idx) in formattedSkinResultData"
-        :key="idx"
-      >
-        <tr>
-          <td class="text-right">
-            {{ result.golferName }}
-          </td>
-          <td class="text-center border-r">
-            {{ result.handicap }}
-          </td>
-          <td
-            v-for="(hole, index) in result.holes"
-            :key="index"
-            class="text-center"
-          >
-            {{ hole.gross }}
-          </td>
-          <td class="border-l">
-            {{ result.grossTotal }}
-          </td>
-        </tr>
+  <div class="mt-4 p-2 mb-3 rounded bg-white border-gray-200 shadow-md overflow-hidden">
+    <table
+      v-if="skinResultData"
+      class="table-auto border-collapse text-sm"
+    >
+      <thead>
         <tr class="border-b">
-          <td class="text-right bg-gray-200">
-            NET
-          </td>
-          <td class="border-r" />
-          <td
-            v-for="(hole, netIndex) in result.holes"
-            :key="netIndex"
-            class="text-center"
+          <th />
+          <th class="text-center px-1">
+            H
+          </th>
+          <th
+            v-for="n in 9"
+            :key="n"
+            class="text-center px-1"
           >
-            {{ hole.net }}
-          </td>
-          <td class="border-l">
-            {{ result.netTotal }}
-          </td>
+            {{ skinResultData.startHole + n - 1 }}
+          </th>
+          <th class="text-center px-1">
+            T
+          </th>
         </tr>
-      </template>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <template
+          v-for="(result,idx) in formattedSkinResultData"
+          :key="idx"
+        >
+          <tr>
+            <td class="text-right">
+              {{ result.golferName }}
+            </td>
+            <td class="text-center border-r">
+              {{ result.handicap }}
+            </td>
+            <td
+              v-for="(hole, index) in result.holes"
+              :key="index"
+              class="text-center"
+            >
+              {{ hole.gross }}
+            </td>
+            <td class="border-l">
+              {{ result.grossTotal }}
+            </td>
+          </tr>
+          <tr class="border-b">
+            <td class="text-right bg-gray-200">
+              NET
+            </td>
+            <td class="border-r" />
+            <td
+              v-for="(hole, netIndex) in result.holes"
+              :key="netIndex"
+              class="text-center"
+            >
+              {{ hole.net }}
+            </td>
+            <td class="border-l">
+              {{ result.netTotal }}
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
 import {computed} from "vue";
