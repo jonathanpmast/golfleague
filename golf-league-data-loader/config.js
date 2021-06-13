8a9e01454c5ebac52e8a1423d888bea21ab64268
Jonathan Mast <jonathanpmast@hotmail.com>
2021-06-13 13:09:18 -0400

    added startYear environment variable

--- golf-league-data-loader/config.js
+++ golf-league-data-loader/config.js
@@ -9,3 +9,4 @@ const rootUrl = process.env.SERVICE_HOST_URL;
 export const configUrl = rootUrl + process.env.LEAGUE_NAME + process.env.CONFIG_SERVICE_PATH;
 export const scoresUrl = rootUrl + process.env.LEAGUE_NAME + process.env.SCORES_SERVICE_PATH;
 export const pathsToExcelWorkbook = process.env.SCOREWORKBOOK_PATHS.split("|");
+export const startYear = process.env.START_YEAR || 2020;
