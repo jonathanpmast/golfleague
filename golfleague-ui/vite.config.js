78a802e5fb3f288cfcf6c897b5583661636058bc
jonathanpmast <jonathanpmast@hotmail.com>
2021-06-17 16:49:39 -0400

    updated some vite settings to enable differen't localhost ports

--- golfleague-ui/vite.config.js
+++ golfleague-ui/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  
+  let _server = {};
+  if(mode=='development') {
+    _server.port = process.env.VITE_SERVER_PORT || 3000
+  }
+  console.log(_server);
+  return defineConfig(
+  {
+    plugins: [vue()],
+    server: _server
+  });
+}
